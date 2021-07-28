fe33aabdeca5a26d40048a802fc80919bee34f0c
I311639 <adriana.stefanova@sap.com>
2021-07-28 17:09:25 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello, penguin\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+	fmt.Println("addr:", addr)
+
+	http.ListenAndServe(addr, nil)
+}
